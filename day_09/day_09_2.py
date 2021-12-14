98b5a5cd8b9b9bc96ede342a224b600eacb332ae
Richard Jackson (laptop) <rjackson87@gmail.com>
2021-12-14 10:48:38 +0000

    Day 09 02 wip

--- day_09/day_09_2.py
+++ day_09/day_09_2.py
@@ -2,6 +2,7 @@ with open("day_09/input.txt", "r+") as f:
     file_contents = f.read().splitlines()
 
 list_of_low_points = []
+
 number_of_rows = len(file_contents)
 number_of_cols = len(file_contents[0])
 
@@ -48,6 +49,7 @@ for index_of_row, value_of_row in enumerate(file_contents):
 
 list_of_basins = []
 
+
 """
 A basin is all locations that eventually flow downward to a single low point. Therefore, every low point has a basin, although some basins are very small. Locations of height 9 do not count as being in any basin, and all other locations will always be part of exactly one basin.
 
@@ -56,26 +58,38 @@ The size of a basin is the number of locations within the basin, including the l
 
 # First off - count how many consecutive points afterwards are successively larger than the preceeding one.
 
-# class LowPoint:
-#     def __init__(self, value, row, col, nadir) -> None:
-#         self.value = value
-#         self.row = row
-#         self.col = col
-#         self.nadir = nadir
 
-# for point in list_of_low_points:
-#     basin_contents = [point]
-#     # points above
-#     top_point = existance_checker(index_of_row - 1, index_of_col)
-#     pointer = LowPoint()
-#     while True:
-
-#         break
+def point_finder(row, col):
+    if row in range(0, number_of_rows + 1) and col in range(0, number_of_cols + 1):
+        return int(file_contents[row][col])
+    else:
+        return None
 
 
 # check all points in orthogonal directions
 
+for point in list_of_low_points:
+    if point.value == 9:
+        continue
+    basin = [point]
+    row_of_point = point.row
+    col_of_point = point.col
 
+    # up:
+    while True:
+        row_of_point -= 1
+        value_of_check = point_finder(row_of_point, col_of_point)
+        if not value_of_check or value_of_check <= point_finder(
+            row_of_point - 1, col_of_point
+        ):
+            list_of_basins.append(basin)
+            break
+        else:
+            basin.append(
+                LowPoint(int(value_of_check), row_of_point, col_of_point, False)
+            )
+
+print(list_of_basins)
 # make each point a +
 # then see how many are adjacent
 # those become SUPER basins
